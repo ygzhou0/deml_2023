fix softmax error

ccs20.py
@@ -31,8 +31,21 @@ embed_layer = model.model.get_input_embeddings()
 
 
 '''the original prompt we try to infer'''
-prompt = "yes sir"
+# prompt = """We have long been expecting you,” said Stepan Arkadyevitch, going into 
+# his room and letting Levin’s hand go as though to show that here all 
+# danger was over. “I am very, very glad to see you,” he went on. “Well, 
+# how are you? Eh? When did you come?” Levin was silent, looking at the unknown faces of Oblonsky’s two 
+# companions, and especially at the hand of the elegant Grinevitch, which 
+# had such long white fingers, such long yellow filbert-shaped nails, and 
+# such huge shining studs on the shirt-cuff, that apparently they 
+# absorbed all his attention, and allowed him no freedom of thought. 
+# Oblonsky noticed this at once, and smiled. “I have the honor of knowing your brother, Sergey Ivanovitch,” said 
+# Grinevitch, holding out his slender hand with its long nails. """
+# prompt = "I fly at least once a month ZRH-LJU-ZRH. My experience is that the staff on the ground need a better education regarding how to care of passengers. If it comes to a delay and pax have to go to the info desk to change tickets and so on then you really see of how unfriendly and unprepared staff are. The airplanes could be cleaned better and I do also see some room for improvement for the flight staff."
+# prompt = "On my Ljubljana - Munich flight in business class Adria used the CRJ-900 Next Generation which is a great plane. I love the very large windows which are at a proper height so that you don't have to bend your neck down in order to look out the window like on the older versions of this Bombardier equipment. Moreover the aircraft is very quiet. It's a short flight but in business class you got a good meal and a comfy seat."
+# 0.5283 precision (1000 epoch), 0.5566 precision (500 epoch)
 
+prompt = "yes sir"
 
 '''get answer's hidden state'''
 with torch.no_grad():
@@ -53,7 +66,6 @@ z.requires_grad_(True)
 # temperature=0.05   # change hyperparameters
 temperature = 1
 
-# set fixed <start> token
 START_EMBED = torch.zeros(tokenizer.vocab_size, 1).type(torch.float16)
 START_EMBED[1][0] = 1    # fix the first token as <start>
 # z = torch.cat((start_embed, z), dim=1)
@@ -67,20 +79,23 @@ START_EMBED[1][0] = 1    # fix the first token as <start>
 
 '''define loss func'''
 loss_func = torch.nn.MSELoss(reduction='mean')
-optim = torch.optim.SGD([z], lr=10000)
+optim = torch.optim.SGD([z], lr=100)
 # optim = torch.optim.Adam([z], lr=0.001)
-scheduler = torch.optim.lr_scheduler.ExponentialLR(optim, gamma=0.995)
+scheduler = torch.optim.lr_scheduler.StepLR(optim, 300,
+                                            gamma=0.2)
 epochs = []
 loss_lst = []
 cos_sim_lst = []
 
-total_epoch = 100
+total_epoch = 1000
 
 for i in range(total_epoch):
     '''forward pass'''
-    sftz = F.softmax(z / temperature, dim=1)
+    sftz = F.softmax(z / temperature, dim=0)
     sftz = torch.cat((START_EMBED.to(sftz.device), sftz), dim=1)    # add a fixed <start> token
+    print('sftz:', sftz)
     new_input_embed = torch.mm(sftz.T, embed_layer.weight)
+    print("new input embed", new_input_embed)
 
 
     '''then I need ||phi(relaxed(Z, T)) - phi(x*)||**2'''
@@ -133,7 +148,7 @@ with torch.no_grad():
 
 
 '''show low confidence word'''
-sftz_conf = F.softmax(z, dim=1)
+sftz_conf = F.softmax(z, dim=0)
 sftz_conf = torch.cat((START_EMBED.to(sftz_conf.device), sftz_conf), dim=1)    # add a fixed <start> token
 print("low confidence:\n",torch.max(sftz_conf, dim=0))
 non_confid_ids = (torch.max(sftz_conf, dim=0).values <= 0.98)
@@ -141,28 +156,28 @@ print(non_confid_ids)
 print(tokenizer.decode(target_input_ids[0][non_confid_ids]))
 
 
-# '''show loss curve'''
-# plt.figure()
-# ax = plt.axes()
-# ax.spines['top'].set_visible(False)
-# ax.spines['right'].set_visible(False)
+'''show loss curve'''
+plt.figure()
+ax = plt.axes()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
 
-# plt.xlabel('epoch')
-# plt.ylabel('loss')
-# plt.plot(epochs, loss_lst, linewidth=1, linestyle='solid', label='L2 loss')
-# plt.legend()
-# plt.title('L2 Loss Curve')
-# plt.savefig("loss-{}-{}-{}-{}-{}-{}.png".format(*time.localtime()))
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.plot(epochs, loss_lst, linewidth=1, linestyle='solid', label='L2 loss')
+plt.legend()
+plt.title('L2 Loss Curve')
+plt.savefig("loss-{}-{}-{}-{}-{}-{}.png".format(*time.localtime()))
 
 
-# plt.figure()
-# ax = plt.axes()
-# ax.spines['top'].set_visible(False)
-# ax.spines['right'].set_visible(False)
+plt.figure()
+ax = plt.axes()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
 
-# plt.xlabel('epoch')
-# plt.ylabel('cosine sim')
-# plt.plot(epochs, cos_sim_lst, linewidth=1, linestyle='solid', label='cosine sim')
-# plt.legend()
-# plt.title('cosine similarity Curve')
-# plt.savefig("cos-sim-{}-{}-{}-{}-{}-{}.png".format(*time.localtime()))
+plt.xlabel('epoch')
+plt.ylabel('cosine sim')
+plt.plot(epochs, cos_sim_lst, linewidth=1, linestyle='solid', label='cosine sim')
+plt.legend()
+plt.title('cosine similarity Curve')
+plt.savefig("cos-sim-{}-{}-{}-{}-{}-{}.png".format(*time.localtime()))

modeling_llama.py
@@ -854,6 +854,8 @@ class LlamaModel(LlamaPreTrainedModel):
             batch_size, seq_length = input_ids.shape
         elif inputs_embeds is not None:
             # print("inference by input embedding")
+            # print("handmade embedding:", inputs_embeds)
+            # NOTE: start embedding already set correct
             batch_size, seq_length, _ = inputs_embeds.shape
         else:
             raise ValueError("You have to specify either input_ids or inputs_embeds")
@@ -874,7 +876,8 @@ class LlamaModel(LlamaPreTrainedModel):
 
         if inputs_embeds is None:
             inputs_embeds = self.embed_tokens(input_ids)
-            print("input embeds", inputs_embeds)
+            # print("input embeds (for first start token)", inputs_embeds)
+            # NOTE: start embedding already right
         # embed positions
         if attention_mask is None:
             attention_mask = torch.ones(
@@ -907,18 +910,20 @@ class LlamaModel(LlamaPreTrainedModel):
         next_decoder_cache = () if use_cache else None
 
         for idx, decoder_layer in enumerate(self.layers):
+            # or to modifiy this, if idx > some certain number, cut the forward process?
+            # cut to 8 layers
+            if idx >= 8:
+                continue
+
+            print(idx, hidden_states)
+
             if output_hidden_states:
                 all_hidden_states += (hidden_states,)
 
             past_key_value = past_key_values[idx] if past_key_values is not None else None
 
+                
             # if self.gradient_checkpointing and self.training:    # modified!
-            # or to modifiy this, if idx > some certain number, cut the forward process?
-            # cut to 8 layers
-            if idx >= 8:
-                continue
-            # print("forward pass layer", idx)
-            # print(decoder_layer)
             if self.gradient_checkpointing:
                 # print("WARNING: using gradient checkpointing")
                 def create_custom_forward(module):