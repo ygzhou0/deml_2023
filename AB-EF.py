ad616c10b5c53f5afed22a7f2335482ef7b4664f
zhou <bjygzhou@126.com>
2023-10-23 15:09:38 +0800

    AB-EF one sentence version

--- AB-EF.py
+++ AB-EF.py
@@ -9,6 +9,7 @@ import numpy as np
 from transformers import (AutoModelForCausalLM, AutoTokenizer)
 
 
+# not only vicuna, I should try other version of llama
 def get_model(devices=['cuda:0'], model_dir="./vicuna-7b-v1.5", 
               model_kwargs={"low_cpu_mem_usage": True, "use_cache": False}):
     tokenizer = AutoTokenizer.from_pretrained(
@@ -45,14 +46,48 @@ def get_hidden_state(tokenizer, model, prompt=None, input_embed=None, target_att
     elif input_embed != None:
         # new_inputs = {'inputs_embeds': input_embed, 'attention_mask': target_attention_mask}
         raise NotImplementedError
-    print("target_input-ids", target_input_ids, len(target_input_ids[0]))
+    # print("target_input-ids", target_input_ids, len(target_input_ids[0]))
     return target_input_ids, target_attention_mask, ori_input_embed, next_.hidden_states
+
+
+def update_weight(weight: torch.Tensor, point, exponential, method="exponential"):
+    assert len(weight.shape) == 1
+    if method == "exponential":
+        if weight[0] >= weight[point]:
+            weight[:point] = weight[:point] * exponential
+            total_value = weight.sum()
+            weight[point:] += (1 - total_value) / (len(weight) - point)
+    elif method == "linear":
+        if weight[0] >= weight[point]:
+            weight[:point] = weight[:point] - exponential * weight[:point]
+            total_value = weight.sum()
+            weight[point:] += (1 - total_value) / (len(weight) - point)
+    else:
+        raise NotImplementedError
+            
+    return weight
+
+
+
+def init_weight_mask(len_cut_output, recover_length, method="exponential", devices=['cuda:0']):
+    if method == "exponential":
+        weight_mask = torch.zeros(len_cut_output + recover_length).type(torch.float16)
+        weight_mask[:recover_length] = 1 / recover_length
+        # print("weight_mask", weight_mask)
+        weight_mask = weight_mask.to(devices[0])
+    elif method == "none":
+        weight_mask = torch.ones(len_cut_output + recover_length).type(torch.float16) / (len_cut_output + recover_length)
+        weight_mask = weight_mask.to(devices[0])
+    else: 
+        raise NotImplementedError
+    return weight_mask
+
 
 
 def main():
     '''get model'''
     devices=['cuda:0']
-    tokenizer, model = get_model(devices=devices)
+    tokenizer, model = get_model(model_dir="./vicuna-7b-v1.5", devices=devices)
 
     '''fix <start> token'''
     embed_layer = model.model.get_input_embeddings()
@@ -78,7 +113,6 @@ def main():
     '''get total output and total hidden'''
     _, target_attention_mask, _, next_hidden_states = get_hidden_state(tokenizer, 
                 model, prompt=total_prompt)
-    print("attention shape", target_attention_mask.shape)
     '''generate total output and total hidden'''
     # target_token = tokenizer(prompt, padding=True, truncation=False, return_tensors='pt')
     # target_input_ids = target_token['input_ids'].to(model.device)
@@ -134,129 +168,146 @@ def main():
     # lr = 1 is not feasible
     # best hyperparameter combination: lr1000, epoch500
     # for cos+cos optimization, best hyperparameter combination is lr 5000, epoch 1000
-    for lr in [1000, 5000, 10000]:
-        for total_epoch in [500, 1000]:
-            for len_cut_output in [1, 5, 10, 20, 40, 60]:
-                '''cut output'''
-                cut_outputs = cut_outputs[:len_cut_output]
-                '''try to init input embed'''
-                size = (len(target_input_ids[0]) - 1, 4096)
-                # means = torch.zeros(size)
-                # new_input_embed = torch.normal(mean=means, std=0.1)
-                # new_input_embed = new_input_embed.type(torch.float16).to(devices[0])
-                
-                new_input_embed_np = np.random.uniform(low=-1., high=1., size=size)
-                # use gaussian distribution may be better
-                # quantify its diffirence. max min mean variance
-                new_input_embed = torch.FloatTensor(new_input_embed_np)
-                new_input_embed = new_input_embed.unsqueeze(dim=0).type(torch.float16).to(devices[0])
-                new_input_embed.requires_grad_(True)
+    for lr in [1000]: # [1000, 5000, 10000]:
+        for total_epoch in [1000]: # [500, 1000]:
+            for len_cut_output in [5, 10, 20, 40]: #[1, 5, 10, 20, 40, 60]:
+                for alpha in [0.001, 0.002, 0.005, 0.01]:
+                    '''cut output "F"'''
+                    cut_outputs_inside = cut_outputs[:,:len_cut_output]
+                    target_attention_mask_inside = target_attention_mask[:,:(len_cut_output + recover_length)]
+                    next_hidden_states_inside = next_hidden_states[:,:(len_cut_output + recover_length)]
+                    # print("inside", cut_outputs_inside.shape, target_attention_mask_inside.shape, next_hidden_states_inside.shape)
+                    '''try to init input embed'''
+                    size = (len(target_input_ids[0]) - 1, 4096)
+                    # means = torch.zeros(size)
+                    # new_input_embed = torch.normal(mean=means, std=0.1)
+                    # new_input_embed = new_input_embed.type(torch.float16).to(devices[0])
+                    
+                    new_input_embed_np = np.random.uniform(low=-1., high=1., size=size)
+                    # use gaussian distribution may be better
+                    # quantify its diffirence. max min mean variance
+                    new_input_embed = torch.FloatTensor(new_input_embed_np)
+                    new_input_embed = new_input_embed.unsqueeze(dim=0).type(torch.float16).to(devices[0])
+                    new_input_embed.requires_grad_(True)
 
 
-                '''optimizer'''
-                optim = torch.optim.SGD([new_input_embed], lr=lr)
-                scheduler = torch.optim.lr_scheduler.ExponentialLR(optim, gamma=0.995)
-                epochs = []
-                loss_lst = []
-                cos_sim_lst = []
-                
-                for i in range(total_epoch):
-                    '''get cut embedding'''
-                    cut_embeds = embed_layer(cut_outputs)
-                    '''add "F" information'''
-                    new_input_embed_ = torch.cat((new_input_embed, cut_embeds), dim=1)
-                    '''add start token'''
-                    new_input_embed_ = torch.cat((START_EMBED.unsqueeze(0), new_input_embed_), dim=1)
-                    '''then I need ||phi(relaxed(Z, T)) - phi(x*)||**2'''
-                    new_inputs = {'inputs_embeds': new_input_embed_, 'attention_mask': target_attention_mask}
-                    phi_relaxed = model(**new_inputs)
+                    '''optimizer'''
+                    optim = torch.optim.SGD([new_input_embed], lr=lr)
+                    # scheduler = torch.optim.lr_scheduler.ExponentialLR(optim, gamma=0.995)
+                    epochs = []
+                    loss_lst = []
+                    cos_sim_lst = []
+
+                    '''implement weighted average of loss idea'''
+                    weight_mask = init_weight_mask(len_cut_output, recover_length, method="exponential", devices=devices)
+                    
+                    # weight_mask = init_weight_mask(len_cut_output, recover_length, method="none", devices=devices)
+                    
+                    for i in range(total_epoch):
+                        '''get cut embedding'''
+                        # cut embeds is proved to be a constant!
+                        cut_embeds = embed_layer(cut_outputs_inside)
+                        '''add "F" information'''
+                        new_input_embed_ = torch.cat((new_input_embed, cut_embeds), dim=1)
+                        '''add start token'''
+                        new_input_embed_ = torch.cat((START_EMBED.unsqueeze(0), new_input_embed_), dim=1)
+                        '''then I need ||phi(relaxed(Z, T)) - phi(x*)||**2'''
+                        new_inputs = {'inputs_embeds': new_input_embed_, 'attention_mask': target_attention_mask_inside}
+                        phi_relaxed = model(**new_inputs)
 
 
-                    '''compute loss'''
-                    loss = loss_func(phi_relaxed.hidden_states, next_hidden_states)
-                    print("{} epoch, {} loss".format(i, loss.data))
+                        '''compute loss'''
+                        # print("hidden states", phi_relaxed.hidden_states, next_hidden_states)
+                        loss = loss_func(phi_relaxed.hidden_states, next_hidden_states_inside)
+                        print("{} epoch, {} loss".format(i, loss.data))
 
 
-                    '''compute similarity'''
-                    cos_sim = F.cosine_similarity(phi_relaxed.hidden_states, next_hidden_states, dim=2)
-                    print("avg cosine sim:", cos_sim.mean())
-                    # print("cosine sim.shape", cos_sim.shape)
-                    cos_sim_lst.append(cos_sim.mean().data.cpu())
+                        '''compute similarity'''
+                        cos_sim = F.cosine_similarity(phi_relaxed.hidden_states, next_hidden_states_inside, dim=2)
+                        # print("avg cosine sim:", cos_sim.mean())
+                        # print("cosine sim.shape", cos_sim.shape, weight_mask.shape)
 
 
-                    '''backward'''
-                    optim.zero_grad()
-                    # loss.backward()
-                    # use cosine similarity as backward function
-                    (-cos_sim).mean().backward()
-                    optim.step()
-                    scheduler.step()
-                    epochs.append(i)
-                    loss_lst.append(loss.data.cpu())
+                        '''backward'''
+                        optim.zero_grad()
+                        # loss.backward()
+                        print(weight_mask)
+                        # use cosine similarity as backward function
+                        sum_cos_sim = ((-cos_sim) * weight_mask).sum()
+                        print("weighted avg cosine sim:", sum_cos_sim)
+                        sum_cos_sim.backward()
+                        # (-cos_sim).mean().backward()
+                        optim.step()
+                        # scheduler.step()
+                        # weight_mask = update_weight(weight_mask, recover_length, 0.999, method="exponential")
+                        weight_mask = update_weight(weight_mask, recover_length, alpha, method="linear")
+                        epochs.append(i)
+                        loss_lst.append(loss.data.cpu())
+                        cos_sim_lst.append(sum_cos_sim.data.cpu())
 
-                # print("after optimized:", cut_outputs)
-                '''show input embedding result'''
-                new_input_embed = new_input_embed.squeeze(0)
-                # print("shapes", embed_layer.weight.shape, new_input_embed.shape)
-                # print("detect nan", torch.any(torch.isnan(new_input_embed)))
-                # print("detect nan", torch.any(torch.isnan(embed_layer.weight)))
-                '''show by L2 distance'''
-                # ret_list = []
-                # for embed in new_input_embed:
-                #     # print("shape", embed.shape, embed_layer.weight.shape)
-                #     dist_ret = torch.norm(embed_layer.weight - embed, p=2, dim=1)
-                #     # print("ret: ", torch.argmin(dist_ret.data.cpu()))
-                #     ret_list.append(torch.argmin(dist_ret.data.cpu()))
-                '''show by cosine similarity'''
-                ret_list = []
-                for j, embed in enumerate(new_input_embed_[0][:recover_length]):
-                    # convert to float32, avoid dividing 0
-                    dist_ret = F.cosine_similarity(embed.type(torch.float32), embed_layer.weight.type(torch.float32)).detach().cpu()
-                    '''test the ranking of wrong tokens--most in top 3'''
-                    print("best position and its cosine value:", torch.argmax(dist_ret.data), torch.max(dist_ret.data))
-                    if torch.argmax(dist_ret.data) != target_input_ids.cpu()[0][j]:
-                        print("correct position and its cosine value:", target_input_ids.cpu()[0][j], dist_ret[target_input_ids.cpu()[0][j]])
-                        print("\n\ntopk value", torch.topk(dist_ret, 10))
-                    ret_list.append(torch.argmax(dist_ret.data))
-                
-                
-                print("ret: ", ret_list, len(ret_list))
-                acc_cnt = 0
-                for j in range(recover_length):
-                    if target_input_ids[0][j] == ret_list[j]:
-                        acc_cnt += 1
-                acc = acc_cnt / len(target_input_ids[0])
-                print("acc: ", acc)
-                ret_tokens = tokenizer.decode(torch.tensor(ret_list))
-                print("final result tokens:", ret_tokens)
-                txt_file.write("lr{}, epoch{}, F length {}, acc{}, final loss{}, cos sim{}, result token: \n{}\n\n\n".format(lr, total_epoch, len_cut_output, acc, loss, cos_sim.mean(), ret_tokens))
-
-
-                '''show loss curve'''
-                plt.figure()
-                ax = plt.axes()
-                ax.spines['top'].set_visible(False)
-                ax.spines['right'].set_visible(False)
-
-                plt.xlabel('epoch')
-                plt.ylabel('loss')
-                plt.plot(epochs, loss_lst, linewidth=1, linestyle='solid', label='L2 loss')
-                plt.legend()
-                plt.title('L2 Loss Curve')
-                plt.savefig("loss-lr-{}-epoch-{}-{}-{}-{}-{}-{}-{}.png".format(lr, total_epoch, *time.localtime()))
+                    # print("after optimized:", cut_outputs)
+                    '''show input embedding result'''
+                    new_input_embed = new_input_embed_.squeeze(0)
+                    # print("shapes", embed_layer.weight.shape, new_input_embed.shape)
+                    # print("detect nan", torch.any(torch.isnan(new_input_embed)))
+                    # print("detect nan", torch.any(torch.isnan(embed_layer.weight)))
+                    '''show by L2 distance'''
+                    # ret_list = []
+                    # for embed in new_input_embed:
+                    #     # print("shape", embed.shape, embed_layer.weight.shape)
+                    #     dist_ret = torch.norm(embed_layer.weight - embed, p=2, dim=1)
+                    #     # print("ret: ", torch.argmin(dist_ret.data.cpu()))
+                    #     ret_list.append(torch.argmin(dist_ret.data.cpu()))
+                    '''show by cosine similarity'''
+                    ret_list = []
+                    for j, embed in enumerate(new_input_embed_[0][:recover_length]):
+                        # convert to float32, avoid dividing 0
+                        dist_ret = F.cosine_similarity(embed.type(torch.float32), embed_layer.weight.type(torch.float32)).detach().cpu()
+                        '''test the ranking of wrong tokens--most in top 3'''
+                        print("best position and its cosine value:", torch.argmax(dist_ret.data), torch.max(dist_ret.data))
+                        if torch.argmax(dist_ret.data) != target_input_ids.cpu()[0][j]:
+                            print("correct position and its cosine value:", target_input_ids.cpu()[0][j], dist_ret[target_input_ids.cpu()[0][j]])
+                            print("\n\ntopk value", torch.topk(dist_ret, 10))
+                        ret_list.append(torch.argmax(dist_ret.data))
+                    
+                    
+                    print("ret: ", ret_list, len(ret_list))
+                    acc_cnt = 0
+                    for j in range(recover_length):
+                        if target_input_ids[0][j] == ret_list[j]:
+                            acc_cnt += 1
+                    acc = acc_cnt / len(target_input_ids[0])
+                    print("acc: ", acc)
+                    ret_tokens = tokenizer.decode(torch.tensor(ret_list))
+                    print("final result tokens:", ret_tokens)
+                    txt_file.write("lr{}, epoch{}, F length {}, alpha {}, acc{}, final loss{}, cos sim{}, result token: \n{}\n\n\n".format(lr, total_epoch, len_cut_output, alpha, acc, loss, sum_cos_sim, ret_tokens))
 
 
-                plt.figure()
-                ax = plt.axes()
-                ax.spines['top'].set_visible(False)
-                ax.spines['right'].set_visible(False)
+                    '''show loss curve'''
+                    plt.figure()
+                    ax = plt.axes()
+                    ax.spines['top'].set_visible(False)
+                    ax.spines['right'].set_visible(False)
 
-                plt.xlabel('epoch')
-                plt.ylabel('cosine sim')
-                plt.plot(epochs, cos_sim_lst, linewidth=1, linestyle='solid', label='cosine sim')
-                plt.legend()
-                plt.title('cosine similarity Curve')
-                plt.savefig("cos-sim-lr-{}-epoch-{}-{}-{}-{}-{}-{}-{}.png".format(lr, total_epoch, *time.localtime()))
+                    plt.xlabel('epoch')
+                    plt.ylabel('loss')
+                    plt.plot(epochs, loss_lst, linewidth=1, linestyle='solid', label='L2 loss')
+                    plt.legend()
+                    plt.title('L2 Loss Curve')
+                    plt.savefig("loss-lr-{}-epoch-{}-{}-{}-{}-{}-{}-{}.png".format(lr, total_epoch, *time.localtime()))
+
+
+                    plt.figure()
+                    ax = plt.axes()
+                    ax.spines['top'].set_visible(False)
+                    ax.spines['right'].set_visible(False)
+
+                    plt.xlabel('epoch')
+                    plt.ylabel('cosine sim')
+                    plt.plot(epochs, cos_sim_lst, linewidth=1, linestyle='solid', label='cosine sim')
+                    plt.legend()
+                    plt.title('cosine similarity Curve')
+                    plt.savefig("cos-sim-lr-{}-epoch-{}-{}-{}-{}-{}-{}-{}.png".format(lr, total_epoch, *time.localtime()))
 
 
 if __name__ == "__main__":
